061a69a429c65a2ca1232ff5c21948f2c2f18094
kebbabb <kebbabb>
2023-10-12 09:54:02 +0200

    ..

--- src/mysketch2.js
+++ src/mysketch2.js
@@ -3,7 +3,11 @@ function setup () {
 }
 
 function draw () {
-  background(0)
+  background(255, 255, 255)
+  fill(234, 2, 32)
+  textAlign(CENTER, CENTER)
+  textSize(33)
+  text('you are gae', 100, 100)
 }
 
 export { setup, draw }
